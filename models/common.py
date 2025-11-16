95bd988912a30567c9b1c1cf6a1ccbb93c5bef14
Linus Lundgren <linus.c.lundgren@gmail.com>
2025-11-16 19:31:25 +0100

    Actor, critic, training steps.

--- models/common.py
+++ models/common.py
@@ -15,20 +15,33 @@ class Actor(nn.Module):
         self.mu = nn.Linear(out_features)
         self.std = nn.Linear(out_features)
 
-    def forward(self, x):
-        proj_x = self.body(x)
-        return self.mu(proj_x), self.std(proj_x)
+    def forward(self, states):
+        proj_states = self.body(states)
+        return self.mu(proj_states), self.std(proj_states)
 
 
 class Critic(nn.Module):
 
-    def __init__(self, in_features, out_features):
+    def __init__(self, in_features):
         self.net = nn.Sequential(
             nn.Linear(in_features, 256),
             nn.ReLU(),
             nn.Linear(256, 256),
             nn.ReLU(),
-            nn.Linear(out_features))
+            nn.Linear(1))
         
-    def forward(self, x):
-        return self.net(x)
+    def forward(self, states, actions):
+        state_actions = torch.concatenate([states, actions], axis=-1)
+        return self.net(state_actions)
+
+
+class DoubleCritic(nn.Module):
+
+    def __init__(self, in_features):
+        self.critic1 = Critic(in_features)
+        self.critic2 = Critic(in_features)
+
+
+    def forward(self, states, actions):
+        state_actions = torch.concatenate([states, actions], axis=-1)
+        return self.critic1(state_actions), self.critic2(state_actions)
